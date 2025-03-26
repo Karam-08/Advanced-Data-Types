5be0f190391af61ea05cadf9cee9498de90c8a0a
TheProgrammerofalltime <kma69362@gmail.com>
2025-03-25 18:21:06 -0700

    commit

---
+++ Mastering 2D Arrays.js
@@ -0,0 +1,39 @@
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+function print2DArray(){
+
+}
+
+function sum2DArray(){
+    
+}
+
+function modify2DArray(row, col, newValue){
+
+}
+
+function findMax(){
+
+}
+
+function transpose(){
+
+}
+
+print2DArray(arr);
+
+
+sum2DArray(arr);
+
+
+modify2DArray(arr);
+
+
+findMax(arr);
+
+
+transpose(arr);
