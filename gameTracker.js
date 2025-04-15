298df3774508663cd77af68196a1bf3d4eb9a31d
TheProgrammerofalltime <kma69362@gmail.com>
2025-04-14 22:10:15 -0700

    comments added

--- gameTracker.js
+++ gameTracker.js
@@ -1,20 +1,34 @@
 let gameTitles = ['Warframe', 'Black Ops Zombies', 'Half Life', 'Portal', 'Roblox', 'Dead Space', 'Fable: The Lost Chapters', 'Fortnite']
-
 let players = ['Karam', 'Hussein', 'Jayden']
 
-playerGames = new Map();
+playerGames = new Map(); 
 
 let gameScores = [
     ['85', '92', '98'],
     ['100', '80', '94'],
     ['78', '92', '85']
-]
+]   
 
-function assignGames(){
-    while(playerGames < 3){
-        randomGame = gameTitles[Math.floor(Math.random() * gameTitles.length)];
-        playerGames.set(players, randomGame)
+function getRandomGames(){
+    let selected = []; // Empty array
+    while(selected.length < 3){ // Sets the amount of games
+        let game = gameTitles[Math.floor(Math.random() * gameTitles.length)]; // A random game is chosen
+        if(!selected.includes(game)){ // If the random game chosen is not already in the array,
+            selected.push(game); // It is pushed into the array
+            console.log(`"${game}" has been added to the player's list.`)
+        }else{
+            console.log(`The player already has "${game}" on their list, so it will be skipped.`)
+        }
+    }
+    return selected;
+}
+
+function assignGames() {
+    for (var i = 0; i < players.length; i++){ // For each player,
+        playerGames.set(players[i], getRandomGames()); // Give each of them 3 random games
     }
 }
+
+assignGames()
 
 console.log(playerGames)
