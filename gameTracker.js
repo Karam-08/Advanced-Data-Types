91e3a2483585c434eae6516f4d21ebbe3f761f5f
TheProgrammerofalltime <kma69362@gmail.com>
2025-04-15 16:05:14 -0700

    commit

--- gameTracker.js
+++ gameTracker.js
@@ -9,7 +9,7 @@ let gameScores = [
     ['100', '80', '94'],
     ['78', '92', '85']
 ]   
-]   
+
 
 function getRandomGames(){
     let selected = []; // Empty array
@@ -28,27 +28,8 @@ function getRandomGames(){
 function assignGames() {
     for (var i = 0; i < players.length; i++){ // For each player,
         playerGames.set(players[i], getRandomGames()); // Give each of them 3 random games
-function getRandomGames(){
-    let selected = []; // Empty array
-    while(selected.length < 3){ // Sets the amount of games
-        let game = gameTitles[Math.floor(Math.random() * gameTitles.length)]; // A random game is chosen
-        if(!selected.includes(game)){ // If the random game chosen is not already in the array,
-            selected.push(game); // It is pushed into the array
-            console.log(`"${game}" has been added to the player's list.`)
-        }else{
-            console.log(`The player already has "${game}" on their list, so it will be skipped.`)
-        }
-    }
-    return selected;
-}
-
-function assignGames() {
-    for (var i = 0; i < players.length; i++){ // For each player,
-        playerGames.set(players[i], getRandomGames()); // Give each of them 3 random games
     }
 }
-
-assignGames()
 
 assignGames()
 
